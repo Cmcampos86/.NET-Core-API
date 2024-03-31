20fafa8e57cd3d6192a80fcf7fd7d5f9e16ea43d
Claudio Marcos de Campos <cmcampos86@gmail.com>
2024-03-31 10:07:47 -0300

    Estudos WebAPI Core

---
+++ PrimeiraAPI/Controllers/TesteController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimeiraAPI.Controllers
+{
+    [ApiController]
+    [Route("api/demo")]
+    public class TesteController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new Produto { Id = 1, Nome = "Teste"});
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(new Produto { Id = 1, Nome = "Teste" }); //200
+        }
+
+        [HttpPost]
+        public IActionResult Post(Produto produto)
+        {
+            return CreatedAtAction("Get", new { id = produto.Id }, produto); //201
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult Put(int id, Produto produto)
+        {
+            if (id != produto.Id) return BadRequest();
+
+            return NoContent(); //204
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
