1113a8c970c7858d71c554dfbf9c3e4b5947309a
Claudio Marcos de Campos <cmcampos86@gmail.com>
2024-04-02 14:12:03 -0300

    Estudos WebAPI Core

---
+++ ApiFuncional/Models/JwtSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiFuncional.Models
+{
+    public class JwtSettings
+    {
+        public string? Segredo { get; set; } //Vai assinar o token
+        public int ExpiracaoHoras { get; set; } 
+        public string? Emissor { get; set; } //Qual a aplicação que esta emitindo o token
+        public string? Audiencia { get; set; } //Em que lugar o token é válido
+    }
+}
